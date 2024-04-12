c4e36628651aae0e42614914636e17da245026ef
Nik <nikhil.murthy.m@gmail.com>
2024-04-12 17:15:11 +1000

    setting up emacs config in a single file to start with

---
+++ init.el
@@ -0,0 +1,97 @@
+;;; Initialize package management
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+;; Fetch the list of packages available 
+(unless package-archive-contents (package-refresh-contents))
+
+
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
+
+
+;;; Theme
+(use-package doom-themes
+:ensure t 
+:init 
+(load-theme 'doom-palenight t))
+
+(use-package heaven-and-hell
+  :ensure t
+  :init
+  (setq heaven-and-hell-theme-type 'dark)
+  (setq heaven-and-hell-themes
+        '((light . doom-acario-light)
+          (dark . doom-palenight)))
+  :hook (after-init . heaven-and-hell-init-hook)
+  :bind (("C-c <f6>" . heaven-and-hell-load-default-theme)
+         ("<f6>" . heaven-and-hell-toggle-theme)))
+
+;;; Some General Settings
+
+;; Disable annoying ring-bell when backspace key is pressed in certain situations
+(setq ring-bell-function 'ignore)
+
+;; Disable scrollbar and toolbar
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+;; Set language environment to UTF-8
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+;; Longer whitespace, otherwise syntax highlighting is limited to default column
+(setq whitespace-line-column 1000) 
+
+;; Enable soft-wrap
+(global-visual-line-mode 1)
+
+;; Maintain a list of recent files opened
+(recentf-mode 1)            
+(setq recentf-max-saved-items 50)
+
+;; Move all the backup files to specific cache directory
+;; This way you won't have annoying temporary files starting with ~(tilde) in each directory
+;; Following setting will move temporary files to specific folders inside cache directory in EMACS_DIR
+(setq user-cache-directory (concat user-emacs-directory "cache"))
+(setq backup-directory-alist `(("." . ,(expand-file-name "backups" user-cache-directory)))
+      url-history-file (expand-file-name "url/history" user-cache-directory)
+      auto-save-list-file-prefix (expand-file-name "auto-save-list/.saves-" user-cache-directory)
+      projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" user-cache-directory))
+
+
+;;; Coding specific setting
+
+;; Automatically add ending brackets and braces
+(electric-pair-mode 1)
+
+;; Make sure tab-width is 4 and not 8
+(setq-default tab-width 4)
+
+;; Highlight matching brackets and braces
+(show-paren-mode 1) 
+
+
+;;; custom-set-variables
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-acario-light))
+ '(custom-safe-themes
+   '("4e2e42e9306813763e2e62f115da71b485458a36e8b4c24e17a2168c45c9cf9d" default))
+ '(package-selected-packages '(heaven-and-hell doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
