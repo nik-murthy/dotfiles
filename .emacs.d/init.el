0351d934c804f62402bf89d336d44f0cc59e2aeb
nik <52d12vjz@duck.com>
2025-05-30 06:42:47 +1000

    cleaning up

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(doom-palenight))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(consult corfu doom-themes exec-path-from-shell heaven-and-hell magit
+	     marginalia moody orderless org-modern org-present ox-hugo
+	     vertico visual-fill-column yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
